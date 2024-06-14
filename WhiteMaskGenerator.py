d4fcccb199d82980f9f22b3bb3772a49c64465e4
baicai99 <1637083533@qq.com>
2024-06-14 16:25:27 +0800

    update

---
+++ WhiteMaskGenerator.py
@@ -0,0 +1,80 @@
+import torch
+
+class WhiteMaskGenerator:
+
+    def __init__(self):
+        """
+        初始化 WhiteMaskGenerator 类。目前不需要特定的初始化操作。
+        """
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        """
+        定义节点的输入类型。
+
+        返回
+        ----
+        dict
+            一个指定所需输入类型的字典。
+        """
+        return {
+            "required": {
+                "num_masks": ("INT", {  # 要创建的蒙版数量的输入类型
+                    "default": 1, 
+                    "min": 1, 
+                    "step": 1, 
+                    "display": "number"
+                }),
+                "mask_height": ("INT", {  # 蒙版的高度
+                    "default": 512,
+                    "min": 1,
+                    "max": 4096,
+                    "step": 1,
+                    "display": "number"
+                }),
+                "mask_width": ("INT", {  # 蒙版的宽度
+                    "default": 512,
+                    "min": 1,
+                    "max": 4096,
+                    "step": 1,
+                    "display": "number"
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("MASK",)  # 输出的类型，将是一个蒙版的张量
+    FUNCTION = "execute"  # 执行的主要函数
+    CATEGORY = "baicai/mask"  # 此节点在UI中出现的类别
+
+    def execute(self, num_masks, mask_height, mask_width):
+        """
+        执行节点的主要功能：创建指定数量的白色蒙版。
+
+        参数
+        ----
+        num_masks : int
+            要创建的蒙版数量。
+        mask_height : int
+            每个蒙版的高度。
+        mask_width : int
+            每个蒙版的宽度。
+
+        返回
+        ----
+        tuple
+            包含生成的白色蒙版张量的元组。
+        """
+        # 创建指定数量的白色蒙版
+        masks = torch.ones((num_masks, mask_height, mask_width), dtype=torch.float32)
+        
+        return (masks,)
+
+# 定义节点类映射和显示名称映射，以便与框架集成
+MASK_CLASS_MAPPINGS = {
+    "WhiteMaskGenerator": WhiteMaskGenerator
+}
+
+MASK_NAME_MAPPINGS = {
+    "WhiteMaskGenerator": "White Mask Generator"
+}
--- __init__.py
+++ __init__.py
@@ -2,17 +2,20 @@
 from .FrameSkipping import FrameSkipping
 from .FrameTruncating import FrameTruncating
 from .MaskFrameSkipping import MaskFrameSkipping
+from .WhiteMaskGenerator import WhiteMaskGenerator
 
 NODE_CLASS_MAPPINGS = {
     "FrameSkipping": FrameSkipping,
     "FrameTruncating": FrameTruncating,  # 添加新的组件到节点类映射
     "MaskFrameSkipping": MaskFrameSkipping,
+    "WhiteMaskGenerator": WhiteMaskGenerator,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FrameSkipping": "Frame Skipping",
     "FrameTruncating": "Frame Truncating",
     "MaskFrameSkipping": "Mask Frame Skipping",  # 添加新的组件到节点显示名称映射
+    "WhiteMaskGenerator": "White Mask Generator",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
